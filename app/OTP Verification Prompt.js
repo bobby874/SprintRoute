f93f06240be3f32d146d04076ca5bbcfc3b6bd79
jxtsammy <robertflames001@gmail.com>
2024-07-13 21:02:12 +0000

    Updated

--- app/OTP Verification Prompt.js
+++ app/OTP Verification Prompt.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import phoneVerification from './OTP Verification'
 
 const OTPprompt = () => {
