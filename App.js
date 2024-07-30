1bf26c030eecbbd23bfe18bc619835a1e9d7fb9c
jxtsammy <robertflames001@gmail.com>
2024-07-30 10:39:04 +0000

    Updates

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import Demo from './app/AppDemo'
 import Options from './app/Options'
 
 const App = () => {
   return (
     <view>
-      <Demo />
       <Options />
     </view>
   )
 }
 
 export default App
-
-
